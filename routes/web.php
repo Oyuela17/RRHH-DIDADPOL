17ce8bacee280c2d17e0dc096617efcf838b7290
Tu Nombre <tunombre@correo.com>
2025-11-11 12:56:27 -0600

    actualizado recientemente

--- routes/web.php
+++ routes/web.php
@@ -96,9 +96,9 @@ Route::get('/', function () {
 // Autenticación de Laravel
 Auth::routes();
 
-// ===============================
+
 // 🔐 RUTAS DE AUTENTICACIÓN
-// ===============================
+
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
